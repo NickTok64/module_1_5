55ff1f0c8b0dd356bde447ba079a8bf6b43cae99
NickTok64 <bignick5515@gmail.com>
2024-11-13 21:24:23 +0300

    Add files via upload

--- module_1_5.py
+++ module_1_5.py
@@ -1,10 +1,14 @@
-mutable_list =['MB', 'BMW', 'Audi', 'VW']
+immutable_var = (['Fiat', 'Alfa Romeo', 'Lancia'],3,True) # итальянские марки неизменны
+print('Итальянские марки автомобилей: ', immutable_var)
+immutable_var[0][2]='Ferrari' # элементы кортежа изменить нельзя, но можно изменить список вложенный в кортеж
+print('Итальянские марки автомобилей: ', immutable_var)
+mutable_list =['MB', 'BMW', 'Audi', 'VW', 4, True]
 print('Немецкие марки автомобилей: ', mutable_list)
-mutable_list.append('Opel') # добавляем Opel
+mutable_list [4:6] = ['Opel', 5, True]# добавляем Opel
 print('Немецкие марки автомобилей: ', mutable_list)
-mutable_list.extend(['Reno', 'Peugeot', 'Citroen']) # добавляем французкие марки
+mutable_list.insert(5, ['Reno', 'Peugeot', 'Citroen']) # добавляем французкие марки
+mutable_list[6]=mutable_list[6] + 3  # пересчитываем кол-во марок
 print('Европейские марки автомобилей: ', mutable_list)
 mutable_list[4] = 'Volvo' # меняем Opel на Volvo
 print('Европейские марки автомобилей: ', mutable_list)
-immutable_tuple = ('Fiat', 'Alfa Romeo', 'Lancia') # итальянские марки неизменны
-print('Итальянские марки автомобилей: ', immutable_tuple)
+
